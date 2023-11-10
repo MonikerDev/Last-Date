c6b75b9f01aacdeac2b6ef91684c011397acfa47
MonikerDev <nnixon2001@gmail.com>
2023-11-09 19:18:33 -0700

    Did some basic work on the inventory system and added database functions with it as far as triggers go. May need a item data storage if they gain more funtionality

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -1,15 +1,27 @@
+using Mono.Data.Sqlite;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
-public abstract class Item
+public class Item : MonoBehaviour
 {
-    public int Quantity { get; set; }
-    public float value { get; set; }
+	public string Name;
+	public Sprite Sprite;
 
-    public Item(int qty, float value)
-    {
-        Quantity = qty;
-        this.value = value;
-    }
+
+
+	//NOT FINSIHED NEEDS TO SET NOT SELECT
+	public void TriggerFlag()
+	{
+		IDbConnection conn = GlobalVariableStorage.CreateAndOpenDatabase();
+
+		SqliteParameter param = new SqliteParameter("$name", Name);
+
+		IDbCommand command = conn.CreateCommand();
+		command.CommandText = "INSERT OR REPLACE INTO YarnBools (key, value) VALUES ($name, 1)";
+		command.Parameters.Add(param);
+
+		command.ExecuteNonQuery();
+	}
 }
