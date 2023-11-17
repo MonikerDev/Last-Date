d910c2f345853f966fae88ea2e3ddfc915f3de44
MonikerDev <nnixon2001@gmail.com>
2023-11-17 12:29:59 -0700

    Added more mockups for design doc

---
+++ Assets/Scripts/Utility/YarnUtilityfunctions.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class YarnUtilityfunctions : MonoBehaviour
+{
+    [YarnCommand("takeFocus")]
+    public void TakeFocus()
+	{
+		//this.transform.
+
+		
+	}
+}
