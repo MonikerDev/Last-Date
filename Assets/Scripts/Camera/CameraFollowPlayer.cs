fbacfa13dbe85422581b0aabf048eccdda7ace1e
MonikerDev <nnixon2001@gmail.com>
2024-02-09 13:59:03 -0700

    Added a proper camera controller that follows the player but also respects edges

---
+++ Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    [Header("Offset Values")]
+    public float xOffSet;
+    public float yOffSet;
+    public GameObject player;
+    private Vector2 cameraOffset;
+
+    [Header("Edge Restrictions")]
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+	private void Update()
+	{
+        cameraOffset = new Vector2(Mathf.Clamp(player.transform.position.x +
+            xOffSet, minX, maxX), Mathf.Clamp(player.transform.position.y + yOffSet, minY, maxY));
+
+        this.transform.position = cameraOffset;
+	}
+}
